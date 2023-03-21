eda2ff554c85b6bec64f50982d99d5eb30964691
Marianna Grammatikopoulou <m.grammatikopoulou@bitmyjob.gr>
2023-03-21 13:08:49 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const pageStyles = {
   color: "#232129",
@@ -126,11 +126,9 @@ const links = [
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
   )
 }
 
