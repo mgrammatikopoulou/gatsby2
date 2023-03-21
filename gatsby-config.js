c8f9dc86bd2ffe9de29195048e4a8a356eea3996
Marianna Grammatikopoulou <m.grammatikopoulou@bitmyjob.gr>
2023-03-21 16:24:24 +0200

    3rd commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
-      }
-    },
+    "gatsby-plugin-react-helmet",
   ],
 }
