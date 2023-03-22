c2b573b5af7cb9d3126fdf9045f50b5b03e27fc9
Marianna Grammatikopoulou <m.grammatikopoulou@bitmyjob.gr>
2023-03-22 10:31:20 +0200

    commit 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    siteUrl: `http://gatsby.bitmyjob.com/`,
-  },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
