2e6ce3c264bf018a09331a50bf2b4c4092662005
Marianna Grammatikopoulou <m.grammatikopoulou@bitmyjob.gr>
2023-03-22 10:20:37 +0200

    4th commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,12 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    siteUrl: `http://gatsby.bitmyjob.com/`,
-  },
   plugins: [
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-       url: process.env.WPGRAPHQL_URL || `http://gatsby.bitmyjob.com/graphql`,
-          },
-        },
+        url: process.env.WPGRAPHQL_URL || `http://gatsby.bitmyjob.com/graphql`,
       },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
     },
   ],
-}
+};
